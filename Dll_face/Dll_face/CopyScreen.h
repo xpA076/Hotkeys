957cf609e0605242158cb03303dcc6feb27cf37f
xpA076 <781398368@qq.com>
2020-02-19 16:04:52 +0800

    submit dll project

---
+++ Dll_face/Dll_face/CopyScreen.h
@@ -0,0 +1,64 @@
+﻿#pragma once
+#include<windows.h>
+#include <opencv2/opencv.hpp>
+
+
+IplImage* ScreenRoiCapture(int x1, int x2, int y1, int y2) {
+
+	LPVOID screenCaptureData = NULL;
+	HBITMAP hBitmap;
+	HDC hDDC;
+	HDC hCDC;
+
+
+
+	int nWidth = x2 - x1;
+	int nHeight = y2 - y1;
+
+	screenCaptureData = new char[nWidth*nHeight * 4];
+	memset(screenCaptureData, 0, nWidth);
+	// Get desktop DC, create a compatible dc, create a comaptible bitmap and select into compatible dc.
+	hDDC = GetDC(GetDesktopWindow());//得到屏幕的dc
+	hCDC = CreateCompatibleDC(hDDC);//
+	hBitmap = CreateCompatibleBitmap(hDDC, nWidth, nHeight);//得到位图
+	SelectObject(hCDC, hBitmap); //好像总得这么写。
+	BitBlt(hCDC, 0, 0, nWidth, nHeight, hDDC, x1, y1, SRCCOPY);
+
+	GetBitmapBits(hBitmap, nWidth*nHeight * 4, screenCaptureData);//得到位图的数据，并存到screenCaptureData数组中。
+	IplImage *img = cvCreateImage(cvSize(nWidth, nHeight), 8, 4);//创建一个rgba格式的IplImage
+	memcpy(img->imageData, screenCaptureData, nWidth*nHeight * 4);//这样比较浪费时间，但写的方便。这里必须得是*4，上面的链接写的是*3，这是不对的。
+
+	return img;
+}
+
+
+IplImage* ScreenRoiCapture(LPRECT lprect) {
+	return ScreenRoiCapture(lprect->left, lprect->right, lprect->top, lprect->bottom);
+}
+
+
+double _GetTemplateCenterPosition(cv::Mat t,int* xpos,int* ypos, int x1, int x2, int y1, int y2 ) {
+	IplImage* img = ScreenRoiCapture(x1, x2, y1, y2);
+	cv::Mat screenroi;
+	cv::cvtColor(cv::cvarrToMat(img), screenroi, CV_BGRA2BGR);
+
+
+
+	cv::Mat match; //result
+	cv::matchTemplate(screenroi, t, match, cv::TM_SQDIFF_NORMED);
+
+	double minVal, maxVal;
+	cv::Point minLoc, maxLoc;
+	cv::minMaxLoc(match, &minVal, &maxVal, &minLoc, &maxLoc);
+
+
+
+	*xpos = minLoc.x + t.cols / 2;
+	*ypos = minLoc.y + t.rows / 2;
+	return minVal;
+
+
+}
+
+
+
---
+++ Dll_face/Dll_face/Dll_face.cpp
@@ -0,0 +1,66 @@
+﻿// Dll_face.cpp : 定义 DLL 应用程序的导出函数。
+//
+
+#include "stdafx.h"
+#include<iostream>
+#include<string>
+#include"CopyScreen.h"
+
+struct MyPoint
+{
+	int x;
+	int y;
+};
+struct MyRect
+{
+	int l;
+	int r;
+	int t;
+	int b;
+};
+struct MyPath
+{
+	int filenum1;
+	int filenum2;
+	int imgnum1;
+	int imgnum2;
+};
+
+
+
+extern "C" _declspec(dllexport) int add(int a, int b) {
+	a = a + 1;
+	return sizeof(a);
+}
+
+
+extern "C" _declspec(dllexport) int test1(MyPoint* ms) {
+	// ms里面的数值不能修改只能读取不知道为啥不过不重要
+	return ms->x + ms->y;
+}
+
+// 返回值为minVal
+extern "C" _declspec(dllexport) double GetImagePosition2(int* px, int* py, MyRect* rect,MyPath* spath,MyRect* mask) {
+	std::string subpath = "";
+	if (spath->filenum2 != 0) {
+		subpath = std::to_string(spath->filenum2) + "\\";
+	}
+
+	std::string strpath = "CapsLock_image\\" + std::to_string(spath->filenum1) + "\\" + subpath;
+	std::string imgname= std::to_string(spath->imgnum1) + "_" + std::to_string(spath->imgnum2) + ".png";
+
+
+	cv::Mat img;
+	img = cv::imread(cv::String(strpath + imgname));
+
+	cv::Mat t;
+	if (mask->r == mask->l) {
+		img.copyTo(t);
+	}
+	else {
+		t = img(cv::Rect(mask->l, mask->t, mask->r - mask->l, mask->b - mask->t));
+	}
+
+	return (_GetTemplateCenterPosition(img, px, py, rect->l, rect->r, rect->t, rect->b));
+
+}
